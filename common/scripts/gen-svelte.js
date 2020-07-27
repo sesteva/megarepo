3c9fd1ffe0041ca699b1fd8689ed021131c3b0d8
Santiago Esteva <santiagoesteva@gmail.com>
2020-07-27 17:59:12 -0500

    added todos on gen-svelte

--- common/scripts/gen-svelte.js
+++ common/scripts/gen-svelte.js
@@ -8,8 +8,11 @@ async function generate() {
   console.log("gen-e2e process: create app-e2e with name: ", `${args[1]}-e2e`);
   await genE2E(appName);
   console.log("installing deps");
-  // await rushUpdate();
 
+  // TODO: make rush update work
+  // await rushUpdate();
+  // TODO: note or automate start script to add -p 5001 5002 5003
+  // TODO: note or automate update rollup config livereload to add new port  !production && livereload({ watch: "public", port: 35730 }),
   console.log("Please run `rush update` at the root of the project");
   console.log("Thats it, happy coding!");
 }
